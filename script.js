7dce5e98cefd40e63405f0eb3503b51a3f70c88d
Weslei Miranda <wsmiranda11@gmail.com>
2024-01-25 19:48:00 -0300

    feat: creates function to encrypt and decrypt text

---
+++ script.js
@@ -0,0 +1,54 @@
+handleEncrypt = () => {
+  action.decodeText(action.encryptText);
+}
+
+handleDecrypt = () => {
+  action.decodeText(action.decryptText);
+}
+
+const action = {
+  encryptText: (text) => {
+    const encryptKey = {
+      'e': 'enter',
+      'i': 'imes',
+      'a': 'ai',
+      'o': 'ober',
+      'u': 'ufat'
+    };
+
+    const encryptText = text.split('').map(char => encryptKey[char] || char).join('');
+
+    return encryptText;
+  },
+
+  decryptText: (text) => {
+    const decryptKey = {
+      'enter': 'e',
+      'imes': 'i',
+      'ai': 'a',
+      'ober': 'o',
+      'ufat': 'u'
+    };
+
+    const regex = new RegExp(Object.keys(decryptKey).join('|'), 'g');
+
+    const decryptText = text.replace(regex, (match) => decryptKey[match]);
+
+    return decryptText;
+  },
+
+  checkText: (text) => {
+    const regex = /^[a-z\s]+$/;
+
+    return regex.test(text)
+  },
+
+  decodeText: (operation) => {
+    let sendField = document.getElementById('main__input__text').value;
+    let receiveField = document.getElementById('main__output__text');
+
+    if (action.checkText(sendField)) {
+      receiveField.innerHTML = operation(sendField);
+    }
+  },
+}
